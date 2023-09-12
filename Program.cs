f8ed9e8b9952266f1855f9ef8aafef52a9865a2d
MadsOrfelt <orfe@itu.dk>
2023-09-12 15:33:56 +0200

    opgave 2.b mangler lidt endnu men ingen fejl længere Co-authored-by: Marius <masto@itu.dk> Jerome <jrah@itu.dk> Oliver <ohag@itu.dk> Casper <cpil@itu.dk>

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.IO;
+﻿
+using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
-using System.Globalization;
-using System.Text;
-using CsvHelper;
-using CsvHelper.Expressions;
+using SimpleDB;
+
+
 
 string command = args[0];
+string message = args[1];
 
 
 
@@ -18,37 +18,10 @@ return new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Local).AddSeconds(epoch).T
     
 
 if (command == "read") {
-try {
-        using (var sr = new StreamReader("chirp_cli_db.csv"))
-        using (var csv = new CsvReader(sr, CultureInfo.InvariantCulture))
-{
-            var records = csv.GetRecords<Cheep>();
-
-            foreach (var record in records) {
-                string time = epoch2String(int.Parse(record.Timestamp.ToString()));
-                Console.WriteLine($"{record.Author} @ " + time + ": " + $"{record.Message}");
-            }
-}
-
-
-} catch (IOException e) {
-    Console.WriteLine("The file could not be read");
-    Console.WriteLine(e.Message);
-}
 } else if(command == "cheep") {
     //https://stackoverflow.com/questions/18757097/writing-data-into-csv-file-in-c-sharp
+    
 
-using(StreamWriter w = File.AppendText("chirp_cli_db.csv")) 
-{       w.WriteLine();
-        var csv = new CsvWriter(w, CultureInfo.InvariantCulture);
-        var author = System.Environment.MachineName;
-        var message =  args[1] ;
-        var timestamp = DateTimeOffset.Now.ToUnixTimeSeconds() + 7200;
-        var line = string.Format("{0},{1},{2}", author, message,timestamp);
-        var cheep = new Cheep(author,message,timestamp); 
-        csv.WriteRecord(cheep);
-        csv.Flush();
-}
 }
 public record Cheep(string Author, string Message, long Timestamp);
 
---
+++ SimpleDB/CSVDatabase.cs
@@ -0,0 +1,51 @@
+﻿namespace SimpleDB;
+
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Expressions;
+using System.IO;
+public sealed class CSVDatabase<T> : IDatabaseRepository<T>
+{
+    public IEnumerable<T> Read(int? limit = null)
+    {
+        try {
+            using (var sr = new StreamReader("chirp_cli_db.csv"))
+            using (var csv = new CsvReader(sr, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<Cheep>();
+                return (IEnumerable<T>)records;
+                /*foreach (var record in records) {
+                    string time = epoch2String(int.Parse(record.Timestamp.ToString()));
+                    Console.WriteLine($"{record.Author} @ " + time + ": " + $"{record.Message}");
+                }*/
+            }
+            } catch (IOException e) {
+                Console.WriteLine("The file could not be read");
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        
+
+    
+    }
+
+    public void Store(T record, string message)
+    {
+        using(StreamWriter w = File.AppendText("chirp_cli_db.csv")) 
+        {       
+            w.WriteLine();
+            var csv = new CsvWriter(w, CultureInfo.InvariantCulture);
+            var author = System.Environment.MachineName;
+            var timestamp = DateTimeOffset.Now.ToUnixTimeSeconds() + 7200;
+            var line = string.Format("{0},{1},{2}", author, message,timestamp);
+            var cheep = new Cheep(author,message,timestamp); 
+            csv.WriteRecord(cheep);
+            csv.Flush();
+        }
+    }
+
+    public void Store(T record)
+    {
+        throw new NotImplementedException();
+    }
+}
