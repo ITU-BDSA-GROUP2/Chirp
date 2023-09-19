a6712c049591e296af34a490382a41399d9f360a
Marius Stokkebro <masto@itu.dk>
2023-09-19 12:11:44 +0200

    made it so our program uses dotcopt library to parse arguments

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using CsvHelper;
 using CsvHelper.Expressions;
 using SimpleDB;
 using DocoptNet;
+using System.Collections.Immutable;
 
 const string usage = @"Chirp
 
@@ -24,18 +25,23 @@ var arguments = new Docopt().Apply(usage, args, exit: true)!;
 string command = args[0];
 
 var db = new CSVDatabase<Cheep>();
-
-
-if (arguments.ContainsKey("read")) {
+if(arguments["read"].Value is bool read)
+{
+  if (read) {
     UserInterface.PrintCheeps(db.Read());
-} else if(arguments.ContainsKey("cheep")) {
-    //https://stackoverflow.com/questions/18757097/writing-data-into-csv-file-in-c-sharp
-    
-    var author = System.Environment.MachineName;
-    var message =  args[1] ;
-    var timestamp = DateTimeOffset.Now.ToUnixTimeSeconds() + 7200;
-    var cheep = new Cheep(author,message,timestamp); 
-    db.Store(cheep);
+  }
+}
+if(arguments["cheep"].Value is bool cheepT)
+{
+  if(cheepT) {
+      //https://stackoverflow.com/questions/18757097/writing-data-into-csv-file-in-c-sharp
+      
+      var author = System.Environment.MachineName;
+      var message =  arguments["<message>"];
+      var timestamp = DateTimeOffset.Now.ToUnixTimeSeconds() + 7200;
+      var cheep = new Cheep(author,message.ToString(),timestamp); 
+      db.Store(cheep);
+  }
 }
 
 
