e4ac25c21dca542fec5dc391983f53b5dfccc702
MadsOrfelt <orfe@itu.dk>
2023-09-19 15:30:32 +0200

    deleted a line of code thats it

--- test/test.Chirp/UnitTest1.cs
+++ test/test.Chirp/UnitTest1.cs
@@ -1,10 +1,22 @@
 namespace test.Chirp;
 
+using Program.cs
+
 public class UnitTest1
 {
-    [Fact]
+    [Theory]
+    [InlineData("Mads Orfelt", "hej med her alle",)]
+    [InlineData("")]
+    [InlineData("Mads Orfelt")]
     public void Test1()
     {
+        //Arrange 
+        string name = "Mads Orfelt"
+        string cheep = "hej med jer alle sammen"
+        string timestamp = ""
+        Program program = new Program()
 
+        //Act
+        program.cheep()
     }
 }
