f20536c3a220b5355cacce09f0751c02cb6392f4
MadsOrfelt <orfe@itu.dk>
2023-09-19 19:45:20 +0200

    "beginning of the unit tests and moved the csv file to the correct folder Oliver Co-authored-by: <ohag@itu.dk> Casper <cpil@itu.dk>"
    "beginning of the unit tests and moved the csv file to the correct folder Oliver Co-authored-by: <ohag@itu.dk> Casper <cpil@itu.dk>"

--- test/test.Chirp/UnitTest1.cs
+++ test/test.Chirp/UnitTest1.cs
@@ -1,22 +1,28 @@
 namespace test.Chirp;
 
 using Program.cs
+using UserInterface.cs
 
 public class UnitTest1
 {
     [Theory]
-    [InlineData("Mads Orfelt", "hej med her alle",)]
-    [InlineData("")]
-    [InlineData("Mads Orfelt")]
-    public void Test1()
+    [InlineData("Mads Orfelt", "hej med her alle",181882832)]
+    [InlineData("Oliver","Gustav er min bedste ven", 99999999)]
+    [InlineData("Casper", "elon er cool", 123444321)]
+    public void Test1(string name, string message, int timestamp)
     {
         //Arrange 
-        string name = "Mads Orfelt"
-        string cheep = "hej med jer alle sammen"
-        string timestamp = ""
         Program program = new Program()
+        var program.cheep = new cheep(name,message,timestamp)
 
         //Act
         program.cheep()
+
+        Assert.Equal()
+    }
+    [Theory]
+    [InlineData()]
+    public void epoch2String() {
+
     }
 }
