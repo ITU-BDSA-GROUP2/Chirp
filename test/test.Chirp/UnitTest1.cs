4424addf246f5a4b4bddc83a0d1677a24b3a464d
MadsOrfelt <orfe@itu.dk>
2023-09-19 21:23:32 +0200

    Update UnitTest1.cs

--- test/test.Chirp/UnitTest1.cs
+++ test/test.Chirp/UnitTest1.cs
@@ -8,7 +8,7 @@ public class UnitTest1
     [Theory]
     [InlineData("Mads Orfelt", "hej med her alle",181882832)]
     [InlineData("Oliver","Gustav er min bedste ven", 99999999)]
-    [InlineData("Casper", "elon er cool", 123444321)]
+    [InlineData("Casper", "Elon Musk er cool", 123444321)]
     public void Test1(string name, string message, int timestamp)
     {
         //Arrange 
@@ -21,8 +21,20 @@ public class UnitTest1
         Assert.Equal()
     }
     [Theory]
-    [InlineData()]
-    public void epoch2String() {
-
+    [InlineData(1690891760)]
+    /*[InlineData(1690978778)]
+    [InlineData(1690979858)]
+    [InlineData(1690981487)]
+    [InlineData(1694517710)]
+    [InlineData(1694517854)]
+    [InlineData(1694521023)]*/
+    public void epoch2String(int epoch) {
+        //Arrange
+        UserInterface user = new UserInterface()
+        
+        //Act
+        var value = user.epoch2String(epoch)
+        //Assert
+        AssertEqual(value,"01-08-2023 12:09:20")
     }
 }
