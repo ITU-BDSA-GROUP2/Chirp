c1f692fb81f7de910a368fb53035475a8767c298
cpilITU <cpil@itu.dk>
2023-09-28 10:31:46 +0200

    testing again

--- src/Chirp.CSVDBService/Program.cs
+++ src/Chirp.CSVDBService/Program.cs
@@ -5,8 +5,8 @@ using System.Text.Json;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var newCheep = new Cheep("Mads","Dette er en test", 1684229348);
-IDatabaseRepository<Cheep> db = CSVDatabase<Cheep>.DBInstance("../../data/chirp_cli_db.csv");
+//var newCheep = new Cheep("Mads","Dette er en test", 1684229348);
+IDatabaseRepository<Cheep> db = CSVDatabase<Cheep>.DBInstance("/chirp_cli_db.csv");
 
 
 app.MapPost("/cheep", (Cheep cheep) => db.Store(cheep));
