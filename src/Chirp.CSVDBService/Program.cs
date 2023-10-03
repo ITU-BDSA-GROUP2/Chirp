36d0ee487cda147ef008ff127bb4270dd512c447
cpilITU <cpil@itu.dk>
2023-10-03 13:15:36 +0200

    Ændr
    Changed file path

--- src/Chirp.CSVDBService/Program.cs
+++ src/Chirp.CSVDBService/Program.cs
@@ -5,7 +5,7 @@ using System.Text.Json;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-IDatabaseRepository<Cheep> db = CSVDatabase<Cheep>.DBInstance("chirp_cli_db.csv");
+IDatabaseRepository<Cheep> db = CSVDatabase<Cheep>.DBInstance("../../data/chirp_cli_db_2.csv");
 
 
 app.MapPost("/cheep", (Cheep cheep) => db.Store(cheep));
