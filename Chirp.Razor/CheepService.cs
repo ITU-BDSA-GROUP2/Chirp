ead952b9e7ed8632c2d2666002f5009506d87d1b
cpilITU <cpil@itu.dk>
2023-10-05 00:56:56 +0200

    Trying to add database again with help from ChatGPT

--- Chirp.Razor/CheepService.cs
+++ Chirp.Razor/CheepService.cs
@@ -39,7 +39,7 @@ public class CheepService : ICheepService
         ORDER BY M.pub_date DESC
         LIMIT 32
         OFFSET {numberOfCheeps};";
-        using (var connection = new SqliteConnection($"Data Source={value}"))
+        using (var connection = new SqliteConnection($"Data Source={chirpPath}"))
         {
             connection.Open();
             var command = connection.CreateCommand();
