21f40dd4d4c1c8a5c0704b121bb9916c49c15062
cpilITU <cpil@itu.dk>
2023-10-11 23:40:53 +0200

    We were able to create cheep and author to database through EF Core and able to read from it. Co-authored-by: Marius <masto@itu.dk> Mads <orfe@itu.dk> Oliver <ohag@itu.dk> Casper <cpil@itu.dk> Jerome <jrah@itu.dk>

--- EFGetStarted/Program.cs
+++ EFGetStarted/Program.cs
@@ -1,11 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 using System.Linq;
+using System.Globalization;
 
-public class Program
+public partial class Program
 {
-    private static void Main(string[] args)
+
+    private static readonly List<CheepViewModel> _cheeps = new List<CheepViewModel>();
+
+    public static void Main(string[] args)
     {
+        var cr = new CheepRepository();
+
+        DateTime time = DateTime.Now;
+
+        var a10 = new Author() { AuthorId = 10, Name = "Jacqualine Gilcoine", Email = "Jacqualine.Gilcoine@gmail.com" };
+
+        var cheep = new Cheep() {
+            CheepId = 1,
+            AuthorId = 1,
+            Author = a10,
+            TimeStamp = DateTime.Now,
+            Text = "Hej"
+        };
+
+        
+
+        cr.add(cheep);
+        var alist = cr.get(0);
+
+        Console.WriteLine(alist.Count);
         
     }
 }
