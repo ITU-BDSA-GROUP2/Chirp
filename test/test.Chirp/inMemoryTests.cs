1cb109ffb71761e20a85de5ec121c7d2c1a0cfb8
MadsOrfelt <orfe@itu.dk>
2023-10-24 14:29:50 +0200

    in memory database tests in progress Co-authored-by: Marius <masto@itu.dk> Jerome <jrah@itu.dk> Oliver <ohag@itu.dk> Casper <cpil@itu.dk>

---
+++ test/test.Chirp/inMemoryTests.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Data.Common;
+using System.Linq;
+//using EF.Testing.BloggingWebApi.Controllers;
+//using EF.Testing.BusinessLogic;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using Chirp.Razor;
+using Infrastructure;
+namespace EF.Testing.UnitTests;
+
+//http://localhost:5273
+
+public class SqliteInMemoryChirpingControllerTest : IDisposable
+{
+    private readonly DbConnection _connection;
+    private readonly DbContextOptions<ChirpDBContext> _contextOptions;
+
+    #region ConstructorAndDispose
+    public SqliteInMemoryChirpingControllerTest()
+    {
+        // Create and open a connection. This creates the SQLite in-memory database, which will persist until the connection is closed
+        // at the end of the test (see Dispose below).
+        _connection = new SqliteConnection("chirpTest.db =:memory:");
+        _connection.Open();
+
+        // These options will be used by the context instances in this test suite, including the connection opened above.
+        _contextOptions = new DbContextOptionsBuilder<ChirpDBContext>()
+            .UseSqlite(_connection)
+            .Options;
+
+        // Create the schema and seed some data
+        using var context = new ChirpDBContext();
+
+        if (context.Database.EnsureCreated())
+        {
+            using var viewCommand = context.Database.GetDbConnection().CreateCommand();
+            viewCommand.CommandText = @"
+                CREATE VIEW AllResources AS
+                SELECT Url
+                FROM Blogs;";
+            viewCommand.ExecuteNonQuery();
+        }
+        context.AddRange(
+            new Author { Name = "Helge", Url = "http://blog1.com/" },
+            new Author { Name = "Rasmus", Url = "http://blog2.com/" });
+        context.SaveChanges();
+    }
+
+    ChirpDBContext CreateContext() => new ChirpDBContext();
+
+    public void Dispose() => _connection.Dispose();
+    #endregion
+
+    #region GetCheep
+    [Fact]
+    public void GetCheep()
+    {
+        using var context = CreateContext();
+        var Author = new AuthorDto("Helge","Helge@wow.dk");
+        var controller = new AuthorRepository(context);
+
+        var blog = controller.GetAuthorByName("Helge");
+
+        Assert.Equal(Author.Name, blog.Name);
+    }
+    #endregion
+
+    // [Fact]
+    // public void GetAllBlogs()
+    // {
+    //     using var context = CreateContext();
+    //     var controller = new BloggingController(context);
+
+    //     var blogs = controller.GetAllBlogs().Value;
+
+    //     Assert.Collection(
+    //         blogs,
+    //         b => Assert.Equal("Blog1", b.Name),
+    //         b => Assert.Equal("Blog2", b.Name));
+    // }
+
+    // [Fact]
+    // public void AddBlog()
+    // {
+    //     using var context = CreateContext();
+    //     var controller = new BloggingController(context);
+
+    //     controller.AddBlog("Blog3", "http://blog3.com");
+
+    //     var blog = context.Blogs.Single(b => b.Name == "Blog3");
+    //     Assert.Equal("http://blog3.com", blog.Url);
+    // }
+
+    // [Fact]
+    // public void UpdateBlogUrl()
+    // {
+    //     using var context = CreateContext();
+    //     var controller = new BloggingController(context);
+
+    //     controller.UpdateBlogUrl("Blog2", "http://blog2_updated.com");
+
+    //     var blog = context.Blogs.Single(b => b.Name == "Blog2");
+    //     Assert.Equal("http://blog2_updated.com", blog.Url);
+    // }
+}
