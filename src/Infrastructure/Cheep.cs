b4376735fbbaa64042cf68051ab699ff73238843
Marius Stokkebro <masto@itu.dk>
2023-10-24 11:18:32 +0200

    added a minimum and maximum amount of characters that a cheep can have

--- src/Infrastructure/Cheep.cs
+++ src/Infrastructure/Cheep.cs
@@ -1,6 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 public class Cheep
 {
     public int CheepId { get; set; }
+    [Required]
+    [StringLength(280, MinimumLength = 1, ErrorMessage = "*message must be between 1 character and 280")]
     public required string Text { get; set; }
     public DateTime TimeStamp { get; set; }
     public int AuthorId { get; set; }
