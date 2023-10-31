a36bed3417694b617fabbece8a806592d91cdd53
Marius Stokkebro <masto@itu.dk>
2023-10-31 13:21:02 +0100

    fixed applicationUser

--- src/Infrastructure/ApplicationUser.cs
+++ src/Infrastructure/ApplicationUser.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 public class ApplicationUser : IdentityUser
 {
+    [Key]
+    public string Email { get; set; }
     public string Name { get; set; }
-    public string E-mail {get; set;}
+
 }
