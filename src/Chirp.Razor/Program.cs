b7c47e7e2ee89f4de9c00e9189587a92b2a47a48
Jerome <jrah@itu.dk>
2023-11-09 14:49:55 +0100

    The server can now run locally on a docker sqlServer, there have been applied changes in program.cs where we now automatically update migrations upon running the program and dbInitializer so that there is no author- or cheepId, this is a prerequisite for running on an sql server.

--- src/Chirp.Razor/Program.cs
+++ src/Chirp.Razor/Program.cs
@@ -4,86 +4,81 @@ using Infrastructure;
 using System.Reflection;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authentication.Cookies;
-public class Program
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Vælg MIT license
+// Opdater eller slet issue workflow 
+// chrip_cli branch 
+
+// Add services to the container.
+//builder.Services.AddDbContext<ChirpDBContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("Chirp")));
+
+
+
+builder.Services.AddRazorPages();
+
+// var folder = Environment.SpecialFolder.LocalApplicationData;
+// var path = Environment.GetFolderPath(folder);
+// var DbPath = System.IO.Path.Join(path, "chirp.db");
+var connectionString = builder.Configuration.GetConnectionString("sqlConnection");
+
+builder.Services.AddDbContext<ChirpDBContext>(
+    options => options.UseSqlServer(
+        connectionString,
+        providerOptions => providerOptions.EnableRetryOnFailure()));                
+        
+builder.Services.AddScoped<ICheepRepository, CheepRepository>();
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+    options.Password.RequiredLength = 8)
+    .AddEntityFrameworkStores<ChirpDBContext>()
+    .AddDefaultUI()
+    .AddDefaultTokenProviders();
+// builder.Services.AddAuthentication(options =>
+    // {
+        // options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        // options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        // options.DefaultChallengeScheme = "GitHub";
+    // })
+    //.AddCookie();
+    // .AddGitHub(o =>
+    // {
+    //     o.ClientId = builder.Configuration["authentication:github:clientId"];
+    //     o.ClientSecret = builder.Configuration["authentication:github:clientSecret"];
+    // });
+
+
+// Tror den skal se således ud
+//builder.Services.AddSingleton<ICheepRepository, CheepRepository>();
+
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
 {
 
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
+    var services = scope.ServiceProvider;
 
-        // Vælg MIT license
-        // Opdater eller slet issue workflow 
-        // chrip_cli branch 
-
-        // Add services to the container.
-        //builder.Services.AddDbContext<ChirpDBContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("Chirp")));
-
-
-
-        builder.Services.AddRazorPages();
-
-        // var folder = Environment.SpecialFolder.LocalApplicationData;
-        // var path = Environment.GetFolderPath(folder);
-        // var DbPath = System.IO.Path.Join(path, "chirp.db");
-        var connectionString = builder.Configuration.GetConnectionString("sqlConnection");
-
-        builder.Services.AddDbContext<ChirpDBContext>(
-            options => options.UseSqlServer(
-                connectionString,
-                providerOptions => providerOptions.EnableRetryOnFailure()));                
-                
-        builder.Services.AddScoped<ICheepRepository, CheepRepository>();
-
-        builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
-         options.Password.RequiredLength = 8)
-         .AddEntityFrameworkStores<ChirpDBContext>()
-         .AddDefaultUI()
-         .AddDefaultTokenProviders();
-        // builder.Services.AddAuthentication(options =>
-            // {
-                // options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                // options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                // options.DefaultChallengeScheme = "GitHub";
-            // })
-            //.AddCookie();
-            // .AddGitHub(o =>
-            // {
-            //     o.ClientId = builder.Configuration["authentication:github:clientId"];
-            //     o.ClientSecret = builder.Configuration["authentication:github:clientSecret"];
-            // });
-
-
-        // Tror den skal se således ud
-        //builder.Services.AddSingleton<ICheepRepository, CheepRepository>();
-
-
-        var app = builder.Build();
-
-        using (var scope = app.Services.CreateScope())
-        {
-
-            var services = scope.ServiceProvider;
-
-            var context = services.GetRequiredService<ChirpDBContext>();
-            DbInitializer.SeedDatabase(context);
-        }
-
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
-
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-
-        app.UseRouting();
-        app.UseAuthentication();
-        app.UseAuthorization();
-        app.MapRazorPages();
-
-        app.Run();
-    }
+    var context = services.GetRequiredService<ChirpDBContext>();
+    context.Database.Migrate();
+    DbInitializer.SeedDatabase(context);
 }
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapRazorPages();
+
+app.Run();
