39c4e43a22adf1b4b12407b3bcd8325a36c128f2
cpilITU <cpil@itu.dk>
2023-11-13 16:47:07 +0100

    Found a workaround to creating github users to database. Changed solution file to find src folder first and then the project (hopefully it works). Writing cheeps is possible now

--- src/Chirp.Razor/Pages/Public.cshtml.cs
+++ src/Chirp.Razor/Pages/Public.cshtml.cs
@@ -7,15 +7,20 @@ namespace Chirp.Razor.Pages;
 
 public class PublicModel : PageModel
 {
-    public readonly ICheepRepository _service;
+    private readonly ICheepRepository _service;
+
+    private readonly IAuthorRepository _authorRepo;
 
     public IEnumerable<CheepDto> Cheeps { get; set; }
 
     public IEnumerable<CheepDto> AllCheeps { get; set; }
 
-    public PublicModel(ICheepRepository service)
+    public string CheepText { get; set; }
+
+    public PublicModel(ICheepRepository service, IAuthorRepository authorRepo)
     {
         _service = service;
+        _authorRepo = authorRepo;
     }
 
     public async Task<ActionResult> OnGet()
@@ -27,9 +32,26 @@ public class PublicModel : PageModel
         return Page();
     }
 
-    public async Task<ActionResult> OnPost() {
-        var cheep = new CheepDto(Request.Form["CheepText"], User.Identity.Name, DateTime.Now);
+    public async Task<ActionResult> OnPost()
+    {
+        var author = User.Identity.Name;
+        var user = await _authorRepo.GetAuthorByName(author);
+        if (user == null) {
+            var authorDto = new AuthorDto(author, author);
+            _authorRepo.CreateNewAuthor(authorDto);
+        }
+
+        string text = Request.Form["CheepText"];
+        Console.WriteLine(text);
+        DateTime timestamp = DateTime.Now;
+        var cheep = new CheepDto(text, author, timestamp);
         _service.CreateCheep(cheep);
+
+        var t = Convert.ToInt32(Request.Query["page"]);
+        if (t > 0) t -= 1;
+        Cheeps = await _service.GetCheeps(t);
+        AllCheeps = await _service.GetAllCheeps();
+        
         return Page();
     }
 }
