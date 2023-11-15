5f06587919e0c4e3e1be286ce329b324c6026f72
cpilITU <cpil@itu.dk>
2023-11-15 19:51:59 +0100

    All warnings should be removed now. Added suppresion operator some places, since they have already been accounted that they can't be null.

--- src/Chirp.Razor/Pages/Public.cshtml.cs
+++ src/Chirp.Razor/Pages/Public.cshtml.cs
@@ -11,11 +11,11 @@ public class PublicModel : PageModel
 
     private readonly IAuthorRepository _authorRepo;
 
-    public IEnumerable<CheepDto> Cheeps { get; set; }
+    public IEnumerable<CheepDto> Cheeps { get; set; } = new List<CheepDto>();
 
-    public IEnumerable<CheepDto> AllCheeps { get; set; }
+    public IEnumerable<CheepDto> AllCheeps { get; set; } = new List<CheepDto>();
 
-    public string CheepText { get; set; }
+    public string? CheepText { get; set; }
 
     public PublicModel(ICheepRepository service, IAuthorRepository authorRepo)
     {
@@ -29,29 +29,31 @@ public class PublicModel : PageModel
         if (t > 0) t -= 1;
         Cheeps = await _service.GetCheeps(t);
         AllCheeps = await _service.GetAllCheeps();
-        return Page();
+        return await showCheeps();
     }
 
     public async Task<ActionResult> OnPost()
     {
-        var author = User.Identity.Name;
-        var user = await _authorRepo.GetAuthorByName(author);
-        if (user == null) {
+            var author = User.Identity!.Name!;
+        if (await _authorRepo.GetAuthorByName(author!) == null) {
             var authorDto = new AuthorDto(author, author);
             await _authorRepo.CreateNewAuthor(authorDto);
         }
 
-        string text = Request.Form["CheepText"];
+        string text = Request.Form["CheepText"]!;
         Console.WriteLine(text);
         DateTime timestamp = DateTime.Now;
         var cheep = new CheepDto(text, author, timestamp);
         await _service.CreateCheep(cheep);
 
+        return await showCheeps();
+    }
+
+    private async Task<ActionResult> showCheeps() {
         var t = Convert.ToInt32(Request.Query["page"]);
         if (t > 0) t -= 1;
         Cheeps = await _service.GetCheeps(t);
         AllCheeps = await _service.GetAllCheeps();
-        
         return Page();
     }
 }
--- src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
@@ -9,9 +9,9 @@ public class UserTimelineModel : PageModel
 {
     private readonly IAuthorRepository _authorRepo;
     private readonly ICheepRepository _service;
-    public IEnumerable<CheepDto> Cheeps { get; set; }
-    public IEnumerable<CheepDto> AllCheeps { get; set; }
-    public string CheepText { get; set; }
+    public IEnumerable<CheepDto> Cheeps { get; set; } = new List<CheepDto>();
+    public IEnumerable<CheepDto> AllCheeps { get; set; } = new List<CheepDto>();
+    public string? CheepText { get; set; }
 
 
 
@@ -24,33 +24,29 @@ public class UserTimelineModel : PageModel
 
     public async Task<ActionResult> OnGet(string author)
     {
-        var t = Convert.ToInt32(Request.Query["page"]);
-        if (t > 0) t -= 1;
-        Cheeps = await _service.GetCheepsFromAuthor(author, t);
-        AllCheeps = await _service.GetAllCheepsFromAuthor(author);
-        return Page();
+        return await showCheeps(author);
        
     }
         public async Task<ActionResult> OnPost() 
         {
-        var author = User.Identity.Name;
-        var user = await _authorRepo.GetAuthorByName(author);
-        if (user == null) {
+            var author = User.Identity!.Name!;
+        if (await _authorRepo.GetAuthorByName(author!) == null) {
             var authorDto = new AuthorDto(author, author);
             await _authorRepo.CreateNewAuthor(authorDto);
         }
 
-        string text = Request.Form["CheepText"];
-        Console.WriteLine(text);
-        DateTime timestamp = DateTime.Now;
-        var cheep = new CheepDto(text, author, timestamp);
+        string text = Request.Form["CheepText"]!;
+        var cheep = new CheepDto(text, author, DateTime.Now);
         await _service.CreateCheep(cheep);
 
+        return await showCheeps(author);
+    }
+
+    private async Task<ActionResult> showCheeps(string author) {
         var t = Convert.ToInt32(Request.Query["page"]);
         if (t > 0) t -= 1;
         Cheeps = await _service.GetCheepsFromAuthor(author, t);
         AllCheeps = await _service.GetAllCheepsFromAuthor(author);
-        
         return Page();
     }
 }
