f82cd9a99e2670d73f9264b15d1d91e8444c5307
Marius Stokkebro <masto@itu.dk>
2023-11-21 13:24:06 +0100

    added login button test co-author: jrah@itu.dk

--- PlaywrightTests/UnitTest1.cs
+++ PlaywrightTests/UnitTest1.cs
@@ -11,13 +11,27 @@ namespace PlaywrightTests;
 public class Tests : PageTest
 {
     [Test]
-    public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage()
+    public async Task HomepageHasChirpAsTitle()
     {
         await Page.GotoAsync("https://bdsagroup2chirprazor.azurewebsites.net/");
 
         // Expect a title "to contain" a substring.
         await Expect(Page).ToHaveTitleAsync(new Regex("Chirp!"));
-
-
     }
+
+    [Test]
+    //This test, tests if our login button leads to our login site
+    public async Task LoginButtonDirectsToLoginPage()
+    {
+        await Page.GotoAsync("https://bdsagroup2chirprazor.azurewebsites.net/");
+
+        var login = Page.GetByRole(AriaRole.Link, new() { Name = "Login" });
+
+        await Expect(login).ToHaveAttributeAsync("href", "/Identity/Account/Login");
+
+        await login.ClickAsync();
+
+        await Expect(Page).ToHaveTitleAsync(new Regex("Log in"));
+    }
+
 }
