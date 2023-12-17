6a222af725c64477cea8ddbac849a0b7148caefd
MrPrip <oliverprip@icloud.com>
2023-12-17 20:49:39 +0100

    Added summeries for our entities (Author, Cheep, Like)

--- src/Infrastructure/Like.cs
+++ src/Infrastructure/Like.cs
@@ -1,5 +1,10 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+
+// <summary>
+//   This class represents a relation between an author and a cheep they liked. 
+// </summary>
+
 public class Like
 {
     [Required]
