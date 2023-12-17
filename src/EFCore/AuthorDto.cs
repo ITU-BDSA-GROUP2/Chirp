74fb08cff7ff119b4ad2fe69a8deb43518971178
MrPrip <oliverprip@icloud.com>
2023-12-18 00:13:54 +0100

    Added summeries for all DTOs

--- src/EFCore/AuthorDto.cs
+++ src/EFCore/AuthorDto.cs
@@ -1,1 +1,4 @@
+// <summary>
+//   This record represents a DTO (Data Transfer Object) for Authors
+// </summary>
 public record AuthorDto(string Name, string Email, int AuthorId);
--- src/EFCore/FollowDto.cs
+++ src/EFCore/FollowDto.cs
@@ -1,1 +1,4 @@
+// <summary>
+//   This record represents a DTO (Data Transfer Object) for Followers
+// </summary>
 public record FollowDto(int UserId, int AuthorId);
