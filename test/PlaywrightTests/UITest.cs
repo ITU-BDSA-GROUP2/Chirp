20a5cb6dcdfa5989ba8ff39ee5442c8ee03e003a
Marius Stokkebro <masto@itu.dk>
2023-12-20 21:36:51 +0100

    test where changed

--- test/PlaywrightTests/UITest.cs
+++ test/PlaywrightTests/UITest.cs
@@ -116,49 +116,20 @@ public class Tests : PageTest
 
         await Page.GetByPlaceholder("Password").FillAsync("Dqa68dfs!");
 
-        await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
-
-        await Page.Locator("li").Filter(new() { HasText = "Jacqualine Gilcoine Follow Starbuck now is what we hear the worst. — 08/01/2023 " }).GetByRole(AriaRole.Button).ClickAsync();
-
-        await Page.GetByRole(AriaRole.Link, new() { Name = "My timeline" }).ClickAsync();
-
-        await Page.Locator("li").Filter(new() { HasText = "Jacqualine Gilcoine Follow Starbuck now is what we hear the worst. — 08/01/2023 " }).GetByRole(AriaRole.Link).ClickAsync();
-
-        await Expect(Page.Locator("h2")).ToHaveTextAsync("Jacqualine Gilcoine's Timeline");
-    }
-    [Test]
-    public async Task UserCanLoginAndUnfollowAnotherUser()
-    {
         await Page.GotoAsync("https://bdsagroup2chirprazor.azurewebsites.net/");
 
-        await Page.GetByRole(AriaRole.Link, new() { Name = "Login" }).ClickAsync();
-
-        await Page.GetByPlaceholder("Name@example.com").ClickAsync();
-
-        await Page.GetByPlaceholder("Name@example.com").FillAsync("cool1@gmail.com");
-
-        await Page.GetByPlaceholder("Password").ClickAsync();
-
-        await Page.GetByPlaceholder("Password").FillAsync("Dqa68dfs!");
-
-        await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
-
-        await Page.Locator("li").Filter(new() { HasText = "Jacqualine Gilcoine Unfollow Starbuck now is what we hear the worst. — 08/01/2023 " }).GetByRole(AriaRole.Button).ClickAsync();
-
-        await Page.GetByRole(AriaRole.Link, new() { Name = "My timeline" }).ClickAsync();
-
-        await Expect(Page.Locator("body")).Not.ToContainTextAsync("Starbuck now is what we hear the worst");
-
+        await Page.Locator("li").Filter(new() { HasText = "Jacqualine Gilcoine Follow Starbuck now is what we hear the worst. — 08/01/2023 " }).GetByRole(AriaRole.Button).First.ClickAsync();
 
     }
+
     [Test]
     public async Task UserCannotFollowIfNotLoggedIn()
     {
         await Page.GotoAsync("https://bdsagroup2chirprazor.azurewebsites.net/");
 
-        await Page.Locator("li").Filter(new() { HasText = "o@o.com Follow Yeet — 11/28/2023 11:26:39 Like 0" }).GetByRole(AriaRole.Button).ClickAsync();
+        await Page.Locator("li").Filter(new() { HasText = "Jacqualine Gilcoine Follow Starbuck now is what we hear the worst. — 08/01/2023 " }).GetByRole(AriaRole.Button).First.ClickAsync();
+        await Expect(Page).ToHaveURLAsync("https://bdsagroup2chirprazor.azurewebsites.net/Identity/Account/Register");
 
-        await Expect(Page.Locator("body")).Not.ToContainTextAsync("o@o.com Unfollow Yeet — 11/28/2023 11:26:39 Like 0");
     }
 
 }
